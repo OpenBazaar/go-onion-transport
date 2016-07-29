2b49e064e7e90dc4bb44b73a0315334b2c09b096
David Stainton <dstainton415@gmail.com>
2016-07-29 10:27:48 +0000

    unfinished work in progress implementing transport interface

--- transport.go
+++ transport.go
@@ -1,45 +1,107 @@
 package onion
 
 import (
-	"fmt"
-	"log"
+	"crypto"
 	"net"
 
 	tpt "github.com/ipfs/go-libp2p-transport"
 	ma "github.com/jbenet/go-multiaddr"
-	manet "github.com/jbenet/go-multiaddr-net"
-	mafmt "github.com/whyrusleeping/mafmt"
-	"golang.org/x/net/context"
+	"github.com/yawning/bulb"
+	"golang.org/x/net/proxy"
 )
 
-var OnionProtocol = ma.Protocol{
-	Code:  444,
-	Name:  "onion",
-	VCode: ma.CodeToVarint(444),
+// OnionTransport implements go-libp2p-transport's Transport interface
+type OnionTransport struct {
+	controlConn *bulb.Conn
+	auth        *proxy.Auth
+	port        uint16
+	key         crypto.PrivateKey
 }
 
-var WsFmt = mafmt.And(mafmt.TCP, mafmt.Base(WsProtocol.Code))
-
-var OnionCodec = &manet.NetCodec{
-	NetAddrNetworks:  []string{"onion"},
-	ProtocolName:     "onion",
-	ConvertMultiaddr: ConvertOnionMultiaddrToNetAddr,
-	ParseNetAddr:     ParseOnionNetAddr,
-}
-
-func init() {
-	err := ma.AddProtocol(OnionProtocol)
+// NewOnionTransport creates a OnionTransport
+// If key is nil then generate a new key.
+func NewOnionTransport(controlNet, controlAddr string, port uint16, key crypto.PrivateKey, auth *proxy.Auth) (*OnionTransport, error) {
+	conn, err := bulb.Dial(controlNet, controlAddr)
 	if err != nil {
-		log.Fatalf("error registering onion protocol: %s", err)
+		return nil, err
 	}
-
-	manet.RegisterNetCodec(OnionCodec)
+	o := OnionTransport{
+		controlConn: conn,
+		auth:        auth,
+		port:        port,
+		key:         key,
+	}
+	return &o, nil
 }
 
-func ConvertOnionMultiaddrToNetAddr(maddr ma.Multiaddr) (net.Addr, error) {
-	// XXX
+func (t *OnionTransport) Dialer(_ ma.Multiaddr, opts ...tpt.DialOpt) (tpt.Dialer, error) {
+	dialer := OnionDialer{
+		auth: t.auth,
+	}
+	return dialer, nil
 }
 
-func ParseOnionNetAddr(a net.Addr) (ma.Multiaddr, error) {
-	// XXX
+func (t *OnionTransport) Listen(a ma.Multiaddr) (tpt.Listener, error) {
+	listener := OnionListener{
+		port: t.port,
+		key:  t.key,
+	}
+	return listener, nil
+}
+
+func (t *OnionTransport) Matches(a ma.Multiaddr) bool {
+	// XXX fix me
+	return true
+}
+
+// OnionDialer implements go-libp2p-transport's Dialer interface
+type OnionDialer struct {
+	auth *proxy.Auth
+}
+
+func (d *OnionDialer) Dial() (tpt.Conn, error) {
+	dialer, err := bulb.Dialer(d.auth)
+	if err != nil {
+		return nil, err
+	}
+	dialer
+}
+
+func (d *OnionDialer) Matches(a ma.Multiaddr) bool {
+	// XXX fix me
+	return true
+}
+
+// OnionListener implements go-libp2p-transport's Listener interface
+type OnionListener struct {
+	port uint16
+	key  crypto.PrivateKey
+}
+
+func (l *OnionListener) Accept() (tpt.Conn, error) {
+}
+
+func (l *OnionListener) Close() error {
+}
+
+func (l *OnionListener) Addr() net.Addr {
+}
+
+func (l *OnionListener) Multiaddr() ma.Multiaddr {
+}
+
+// OnionConn implement's go-libp2p-transport's Conn interface
+type OnionConn struct {
+	transport *OnionTransport
+	conn      *bulb.Conn
+}
+
+func (c *OnionConn) Transport() {
+	return c.transport
+}
+
+func (c *OnionConn) LocalMultiaddr() ma.Multiaddr {
+}
+
+func (c *OnionConn) RemoteMultiaddr() ma.Multiaddr {
 }
